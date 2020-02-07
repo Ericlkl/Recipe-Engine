6396c05b37ac92fb1aad76995278f15409b97deb
Eric Lee <Raidenlkl@gmail.com>
2020-02-07 18:30:52 +1000

    Search Fruit API

---
+++ server/src/api/index.ts
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+// Create a Food Data Central API server
+export const foodDataCentralAPI = axios.create({
+  baseURL: `https://api.nal.usda.gov/fdc/v1/search`
+});
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,30 @@
 import _ from 'lodash';
+import express, { json } from 'express';
+import path from 'path';
+
+const app = express();
+app.use(json());
+
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
 import { storeInfoToCSV } from './services/csvhelper';
+import { foodDataCentralAPI } from './api';
 
 storeInfoToCSV({
   Fruit: 'Apple',
   Recipe: 'Banana',
   Ingredients: 'Hello'
 });
+
+const identifyFruit = async (name: string) => {
+  const res = await foodDataCentralAPI.get('/', {
+    params: {
+      api_key: process.env.DATA_CENTRAL_APIKEY,
+      generalSearchInput: 'Banana'
+    }
+  });
+
+  console.log(res.data);
+};
+
+identifyFruit('Banana');
